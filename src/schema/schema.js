b59cc67cfbb63838dca49fb8c79609050674ff79
Ricardo kenji Vivas Maeda <ricardo.maeda@veget.com.br>
2021-12-01 10:27:08 -0300

    my first  mutation

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -4,26 +4,44 @@ import {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
 } from "graphql";
 
 const CompanyType = new GraphQLObjectType({
   name: "CompanyType",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      //aqui e relacao de 1 para N entre Company e User
+      //pode ter muitos users e uma company
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        //nosso users nao tem args, entao nao precisamos passar nada
+        return (
+          api
+            //no resolve, pega o id da company e busca os users relacionados
+            //seria fazer no navegador /companies/3/users
+            .get(`/companies/${parentValue.id}/users`)
+            .then((res) => res.data)
+        );
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: "UserType",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       //parentValue retorna o objeto que está sendo acessado
+      //aqui e relacao de 1 para 1, so pode ter um usuario por empresa
       resolve(parentValue, args) {
         return (
           api
@@ -34,7 +52,7 @@ const UserType = new GraphQLObjectType({
         );
       },
     },
-  },
+  }),
 });
 
 //este root query e quem fara a consulta no banco de dados
@@ -45,8 +63,34 @@ const RootQuery = new GraphQLObjectType({
     users: {
       type: UserType,
       args: { id: { type: GraphQLString } },
+      //de preferencia usar args no RootQuery
       resolve(parentValue, args) {
         return api.get(`/users/${args.id}`).then((res) => res.data);
+      },
+    },
+    companies: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return api.get(`/companies/${args.id}`).then((res) => res.data);
+      },
+    },
+  },
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      //id e auto gerado pelo graphql
+      resolve(parentValue, { firstName, age }) {
+        return api.post("/users", { firstName, age }).then((res) => res.data);
       },
     },
   },
@@ -54,4 +98,5 @@ const RootQuery = new GraphQLObjectType({
 
 export default new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
